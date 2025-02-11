aff9aebae0ec79cd6cae5493affe94803d47d8d3
dikie001 <omondidickens255@gmail.com>
2025-02-11 10:50:39 +0300

    add interactive display for data recovery details in Hero and Recovery components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Hero.css';
+import { FaTimes } from 'react-icons/fa';
+
 
 const Hero = () => {
+  const [isClicked, setIsClicked] = useState(false)
+  const seeMore =()=>{
+    setIsClicked(!isClicked)
+  }
   return (
     <div className='hero '>
         <div className="container">
@@ -9,7 +15,21 @@ const Hero = () => {
                 <h1 className='font-semibold'>Data</h1>
                 <h1 className='blue font-semibold'>Secured</h1>
                 <h1 className='font-semibold'>Protection</h1>
-                <div><button>See More</button></div>
+                <div><button onClick={seeMore}>See More</button></div>
+                <div className={isClicked? "display":"nullDisplay"}>
+                  <FaTimes className='icon' onClick={()=>setIsClicked(!isClicked)}/>
+                    <div className="content">
+                  <h2 className='font-semibold text-white relative'>Bringing Lost Data Back to Life</h2>
+                  <p className='text-white font-semibold'>Your Data is valuable, and losing it can be frustrating, if not 
+                    devastating. Thats why <span>Secured</span> is here to provide fast 
+                    secure, and reliable data recovery solutions. Whether it's personal files
+                    or business data, we help you get what matters most. <br />
+                    Need help? We've got You covered
+                    <button>Get Started</button>
+                    
+                  </p>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
--- src/components/Recovery/Recovery.jsx
+++ src/components/Recovery/Recovery.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-import './Recovery.css'
-
+import React, { useState } from "react";
+import "./Recovery.css";
+import { FaTimes } from "react-icons/fa";
 
 const Recovery = () => {
-  return <>
-  
-    <div className="recovery">
-    <div className="container">
-        <div className="content">
-            <h2 className="font-semibold"><span>Data</span> Recovery</h2>
-            <p>Reach out to us today to recover your precious data. 
-                We retrieve data even from the worst case scenarios.
+  const [isClicked, setIsClicked] = useState(false);
+  const learnMore = () => {
+    setIsClicked(!isClicked);
+  };
+  return (
+    <>
+      <div className="recovery">
+        <div className="container">
+          <div className="content">
+            <h2 className="font-semibold">
+              <span>Data</span> Recovery
+            </h2>
+            <p>
+              At Secured, we specialize in professional sata recovery solutions,
+              helping individuals and businesses retrieve lost files with
+              efficiency, security, and reliability.
+              <br />
+              Whether your data loss is due to accidental deletion, hardware
+              failuer, corruption, or malware, our advanced recovery techniques
+              ensure the best possible outcome.
             </p>
             <div>
-                <button>Learn More</button>
+              <button onClick={learnMore}>Learn More</button>
             </div>
+            <div className={isClicked ? "display" : "nullDisplay"}>
+              <FaTimes className='icon' onClick={()=>setIsClicked(!isClicked)}/>
+                <div className="content">
+              <h2 className="font-bold">Why Choose Us?</h2>
+              <p className="font-semibold">
+                1. <span className="">Expert Recovery-</span> Advanced tools for various storage devices <br />
+                2. <span>Secure and Confidential-</span> Your data privacy is our Priority <br />
+                3. <span>Fast and Reliable-</span> Minimize downtime with efficient recovery
+              </p>
+          
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-
-  </div>
-  </>;
+      </div>
+    </>
+  );
 };
 export default Recovery;
